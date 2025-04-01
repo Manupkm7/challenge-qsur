af835a8ca92b89783667cc152620358d0f2e5d40
Manupkm7 <espinozavictormanuel76@gmail.com>
2025-04-01 12:17:46 -0300

    first configuration

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-full mx-auto px-2 max-w-full mt-2'>
+        <h1 className='text-3xl font-bold text-red-500'>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
