edadfd8791b5f2e0bc3d06ebdeee5d5dcc1506b1
Manupkm7 <espinozavictormanuel76@gmail.com>
2025-04-01 15:19:52 -0300

    final configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,8 @@
-import replace from 'rollup-plugin-re';
-
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  base: '/',
-  plugins: [react(), tailwindcss(), tsconfigPaths(),
-  replace({
-    patterns: [
-      {
-        match: /js-sha256/,
-        test: `eval("require('crypto')")`,
-        replace: `require('crypto')`,
-      },
-      {
-        match: /js-sha256/,
-        test: `eval("require('buffer').Buffer")`,
-        replace: `require('buffer').Buffer`,
-      },
-    ],
-  }),],
-
+    plugins: [react(), tsconfigPaths()],
 })
